b52bde31242b860c42940594ae1654e901847971
Logan <95258701+Lash12@users.noreply.github.com>
2026-06-19 13:10:54 -0400

    fix(optiplex): initialize database against service host

--- .docker/data/init.sh
+++ .docker/data/init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "===== INITIALIZING (if this fails, run again until it works) ====="
 cd /home/airline/airline/airline-data
-sbt publishLocal
+SBT_OPTS="-Xmx1536M -Xms512M -XX:MaxMetaspaceSize=512M ${SIM_EXTRA_OPTS:-}" sbt publishLocal
 echo "===== STARTING MIGRATION ====="
 for i in `seq 1 5`
 do
-  sbt "runMain com.patson.init.MainInit"
+  SBT_OPTS="-Xmx1536M -Xms512M -XX:MaxMetaspaceSize=512M ${SIM_EXTRA_OPTS:-}" sbt "runMain com.patson.init.MainInit"
   if [ $? -eq 0 ]; then
     echo "Command succeeded on attempt $i"
     break
