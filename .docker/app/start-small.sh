2685e629ac5392d6275c29011aa232014a1bb3a9
Logan <95258701+Lash12@users.noreply.github.com>
2026-06-19 23:51:34 -0400

    fix(deploy): make app data volume writable

--- .docker/app/start-small.sh
+++ .docker/app/start-small.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -eu
 
+if [ "$(id -u)" = "0" ]; then
+  mkdir -p /home/airline/data
+  chown -R airline:airline /home/airline/data
+  exec sudo -E -u airline env HOME=/home/airline sh /home/airline/start-small.sh
+fi
+
 echo "===== START OF SMALL-SERVER APP SUPERVISOR ====="
 
 wait_for_db() {
